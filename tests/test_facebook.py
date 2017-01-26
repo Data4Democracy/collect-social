bc15ccb09fc306ec9cb24c3a8d83838ad2197624
Kyler Brown <kylerjbrown@gmail.com>
2017-01-25 21:03:34 -0600

    first tests and travis ci

---
+++ tests/test_facebook.py
@@ -0,0 +1,30 @@
+import pytest
+import collect_social
+from collect_social.facebook import utils
+
+@pytest.fixture
+def tmp_db_str(tmpdir):
+    ''' Creates a temporary database connection string.'''
+    return 'sqlite:///{}'.format(tmpdir.join('test.sqlite').strpath)
+
+@pytest.fixture
+def db(tmp_db_str):
+    ''' Creates a temporary database.'''
+    return utils.setup_db(tmp_db_str)
+
+def test_setup_db_tables(db):
+    for table in ['page', 'user', 'post', 'comment', 'interaction']:
+        assert table in db
+
+def test_update_user_insert(db):
+    user_data = dict(id=451, name='Ray Bradbury')
+    utils.update_user(db, user_data)
+    assert db['user'].find_one(user_id=user_data['id'])['name'] == user_data['name']
+
+def test_update_user_no_duplicates(db):
+    user_data = dict(id=314, name='Pi')
+    utils.update_user(db, user_data)
+    utils.update_user(db, user_data)
+    assert db['user'].count() == 1
+
+
