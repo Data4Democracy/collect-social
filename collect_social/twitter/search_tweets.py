c2a109555dc2e66311f6cdac860e92285462ddbb
Michael J Erwin <michaeljameserwin@gmail.com>
2017-01-27 05:12:06 +0000

    Basic twitter functionality

--- collect_social/twitter/search_tweets.py
+++ collect_social/twitter/search_tweets.py
@@ -1,5 +1,6 @@
 import tweepy
 import dataset
+from tweet_model import map_tweet
 
 # unicode mgmt
 import sys
@@ -7,39 +8,42 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 
 # auth & api handlers
-auth = tweepy.OAuthHandler(config.CONSUMER_KEY, config.CONSUMER_SECRET)
-auth.set_access_token(config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET)
-api = tweepy.API(auth)
+def get_api(consumer_key,consumer_secret,access_token,access_token_secret):
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    return tweepy.API(auth)
 
-# load topics & build a search
-topics = ["alted"]
-search = api.search(q=topics, count=100)
+
+def sqlite_connect(connection_string):
+    return dataset.connect(connection_string)
+
+
+def setup_sqlite(db):
+    tweet_table = db['tweet']
+    tweet_table.create_index(['id_str'])
+    tweet_table.create_index(['user_id'])
+
 
 def setup_db(connection_string):
     db = sqlite_connect(connection_string)
     setup_sqlite(db)
     return db
 
-def sqlite_connect(connection_string):
-    return dataset.connect(connection_string)
 
-def setup_sqlite(db):
+def upsert_tweets(tweets,db,topics):
+    if not tweets:
+        return None
+
     tweet_table = db['tweet']
-    media_table = db['media']
-    mention_table = db['mention']
-    url_table = db['url']
-    hashtag_table = db['hashtag']
-    user_table = db['user']
+    
+    for tweet in tweets:
+        if (not tweet.retweeted) and ('RT @' not in tweet.text):
+            tweet_table.upsert(map_tweet(tweet,topics), ["id_str"])        
 
-    tweet_table.create_index(['tweet_id'])
-    media_table.create_index(['tweet_id'])
-    mention_table.create_index(['tweet_id'])
-    url_table.create_index(['tweet_id'])
-    hashtag_table.create_index(['tweet_id'])
 
-    tweet_table.create_index(['user_id'])
-    media_table.create_index(['user_id'])
-    mention_table.create_index(['user_id'])
-    mention_table.create_index(['mentioned_user_id'])
-    url_table.create_index(['user_id'])
-    hashtag_table.create_index(['user_id'])
+def run(consumer_key,consumer_secret,access_token,access_token_secret,connection_string,topics,count=100):
+    api = get_api(consumer_key,consumer_secret,access_token,access_token_secret)
+    search = api.search(q=topics, count=count)
+    db = setup_db(connection_string)
+
+    upsert_tweets(search,db,topics)
