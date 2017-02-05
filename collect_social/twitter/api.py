afd2f2f9636b9a79935de5b330ea65cd7ebe8680
bridget <bkey23@gmail.com>
2017-02-04 22:26:47 -0500

    get user timeline

--- collect_social/twitter/api.py
+++ collect_social/twitter/api.py
@@ -19,18 +19,45 @@ def map_user(twitter_user):
     }
 
 
-def map_followers(twitter_followers):
-    return [map_user(follower) for follower in twitter_followers]
+def map_posts(twitter_posts):
+    return [map_post(twitter_post) for twitter_post in twitter_posts]
 
 
-def map_following(twitter_following):
-    # return d4d.Following
-    pass
+def map_users(twitter_users):
+    return [map_user(user) for user in twitter_users]
 
 
 def map_post(twitter_post):
-    # return d4d.Post
-    pass
+    return {
+        'user': map_user(twitter_post.user),
+        'message': twitter_post.text,
+        'tweet_id': twitter_post.id_str,
+        'language': twitter_post.lang,
+        'post_created': twitter_post.created_at,
+        'geo_coordinates': twitter_post.coordinates,
+        'hashtags': [hashtag['text'] for hashtag in twitter_post.entities.get('hashtags')],
+        'user_mentions': [user['screen_name'] for user in twitter_post.entities.get('user_mentions')],
+        'urls': [url['expanded_url'] for url in twitter_post.entities.get('urls')],
+        'favorites_counts': twitter_post.favorite_count,
+        'retweets_count': twitter_post.retweet_count,
+        'in_reply_to_screen_name': twitter_post.in_reply_to_screen_name,
+        'in_reply_to_status_id': twitter_post.in_reply_to_status_id_str,
+        'is_retweet': twitter_post.retweeted,
+        'place': map_place(twitter_post.place )if twitter_post.place else None
+    }
+
+
+def map_place(place):
+    return {
+        'place_id': place.id,
+        'attributes': place.attributes,
+        'country': place.country,
+        'country_code': place.country_code,
+        'full_name': place.full_name,
+        'name': place.name,
+        'place_type': place.place_type,
+        'url': place.url
+    }
 
 
 def map_comment(twitter_post):
@@ -63,15 +90,19 @@ class Twitter(object):
         Params: id[ ]
         Returns: dict[ ]
         """
-        pass
+        twitter_posts = []
+        for page in tweepy.Cursor(self._api.user_timeline, id=user_id).pages():
+            twitter_posts.extend(page)
+
+        return map_posts(twitter_posts)
 
     def get_user(self, handle):
         """
         Params: id
         Returns: dict
         """
-        tweepy_user = self._api.get_user(handle)
-        return map_user(tweepy_user)
+        twitter_user = self._api.get_user(handle)
+        return map_user(twitter_user)
 
     def get_users(self, id_list):
         """
@@ -89,13 +120,18 @@ class Twitter(object):
         for page in tweepy.Cursor(self._api.followers, id=user_id).pages():
             followers.extend(page)
 
-        return map_followers(followers)
+        return map_users(followers)
 
     def get_following(self, user_id):
         """
         Params: id
         Returns: dict[ ]
         """
+        following = []
+        for page in tweepy.Cursor(self._api.friends, user_id).pages():
+            following.extend(page)
+
+        return map_users(following)
 
     def get_comments(self, post_id):
         """
--- tests/test_twitter_api.py
+++ tests/test_twitter_api.py
@@ -1,11 +1,20 @@
 import pytest
-from collect_social.twitter.api import map_user, map_followers
+from collect_social.twitter.api import map_user, map_users, map_post, map_place, map_posts
 
 
 class User:
     def __init__(self, **entries):
         self.__dict__.update(entries)
 
+
+class Status:
+    def __init__(self,**entries):
+        self.__dict__.update(entries)
+
+
+class Place:
+    def __init__(self,**entries):
+        self.__dict__.update(entries)
 
 @pytest.fixture
 def twitter_user():
@@ -30,9 +39,79 @@ def twitter_user():
     return user
 
 
+@pytest.fixture
+def tweet_post():
+    tweet_dict = {
+        'user': twitter_user(),
+        'created_at': '2017-01-30 03:03:01.000000',
+        'id_str': '825775143846871041',
+        'retweeted': False,
+        'retweet_count': 5,
+        'favorite_count': 5,
+        'lang': 'en',
+        'coordinates': None,
+        'entities': {
+            'hashtags': [
+                {'text': 'data4democracy'}
+            ],
+            'user_mentions': [
+                {
+                    'screen_name': 'realDonaldTrump'
+                },
+                {
+                    'screen_name': 'BarackObama'
+                }
+            ],
+            'urls': [
+                {'expanded_url': 'http://www.datafordemocracy.org'}
+            ]
+        },
+        'in_reply_to_screen_name': None,
+        'in_reply_to_status_id_str': None,
+        'place': None,
+        'text': 'Check out Collect Social, a tool for grabbing posts and comments from Facebook, along with Twitter search data.'
+    }
+
+    tweet = Status(**tweet_dict)
+    return tweet
+
+
+@pytest.fixture
+def twitter_place():
+    place_dict = {
+        'id': '7238f93a3e899af6',
+        'attributes': {
+            'street_address': '795 Folsom St',
+            '623:id': '210176',
+            'twitter': 'twitter'
+        },
+        'country': 'USA',
+        'country_code': 'US',
+        'full_name': 'Whereverville, CA',
+        'name': 'Whereverville',
+        'place_type': 'city',
+        'url': 'https://api.twitter.com/1.1/geo/id/7238f93a3e899af6.json'
+    }
+
+    place = (Place(**place_dict))
+    return place
+
+
 def test_map_user():
     assert map_user(twitter_user())
 
 
-def test_map_followers():
-    assert map_followers([twitter_user()])
+def test_map_users():
+    assert map_users([twitter_user()])
+
+
+def test_map_post():
+    assert map_post(tweet_post())
+
+
+def test_map_posts():
+    assert map_posts([tweet_post()])
+
+
+def test_map_place():
+    assert map_place(twitter_place())
