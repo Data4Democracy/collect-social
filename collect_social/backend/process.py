4a949d4e62c8d4bd808c726f65baf21e1030505c
Ahmad Ragab <ASRagab@Ragab-MBP.local>
2017-02-14 23:15:43 -0500

    Eventador Async Refactor of Twitter Refactor Branch

--- collect_social/backend/process.py
+++ collect_social/backend/process.py
@@ -1,19 +1,16 @@
-from collect_social import config
+from collect_social import config_example
 import json
 
 ## backend setup class? 
 
+async def process_batch(batch):
 
-def process_batch(batch):
-
-    if 'file' in config.storage.keys():
-        file_path = config.storage['file']
+    config = config_example.config
+    if 'file' in config.keys():
+        file_path = config['file']
 
         with open(file_path, 'a') as f:
-            for item in batch:
-                json.dump(item._json, f)
-                f.write('\n')
-                print(item.text)
+            f.writelines([json.dumps(item._json) + ',\n' for item in batch])
 
     # if 's3' in config.storage.keys():
         # set aws creds
