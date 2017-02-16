b42e1a0b895917b20c8d490be5deb8e05d867669
James Westover <james.westover@here.com>
2017-02-15 20:57:04 -0800

    Updating setup.py and requirements.txt to be more library like

--- setup.py
+++ setup.py
@@ -4,4 +4,10 @@ setup(name='collect_social',
       packages=find_packages(),
       zip_safe=False,
       license='MIT',
+      install_requires=['facepy',
+'dataset',
+'tweepy',
+'pytest==3.0.6',
+'coverage',
+'pytest-cov==2.4.0']
       )
