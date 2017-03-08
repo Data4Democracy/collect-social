c5b4eac4f88002fcd6f065b918760efd8fc2c107
ccarey <cemcarey@gmail.com>
2017-03-07 20:00:41 -0600

    adding historial tweets feature using selenium

--- collect_social/twitter/get_tweets.py
+++ collect_social/twitter/get_tweets.py
@@ -5,27 +5,141 @@ import twitter
 import sys
 import time
 
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException 
+from selenium.common.exceptions import StaleElementReferenceException
+
 from collect_social.twitter.utils import get_api
-from datetime import datetime
+import datetime
 
+def chunker(seq, size):
+    """
+    Taken from:
+    http://stackoverflow.com/questions/434287/what-is-the-most-pythonic-way-to-iterate-over-a-list-in-chunks
+    """
+    return (seq[pos:pos + size] for pos in xrange(0, len(seq), size))
 
-def get_tweets(api,user_id,max_id=None):
-    kwargs = {
-        'user_id':user_id,
-        'count':200
-    }
+def increment_day(date, i):
+    """Increment day object by i days.
+    
+    Taken from:
+    https://github.com/alejandrox1/tweet_authorship/blob/master/Notebook_helperfunctions.py
 
-    if max_id is not None:
-        kwargs['max_id'] = max_id
+    Params
+    -------
+    date : datetime-obj
+    i : int
+    
+    Returns
+    -------
+    {datetime-obj} next day.
+    """
+    return date + datetime.timedelta(days=i)
 
-    try:
-        tweets = api.GetUserTimeline(**kwargs)
-    except Exception, e:
-        print(kwargs)
-        print(e)
-        return []
-    return tweets
+def twitter_url(screen_name, no_rt, start, end):
+    """Form url to access tweets via Twitter's search page.
 
+    Taken from: 
+    https://github.com/alejandrox1/tweet_authorship/blob/master/Notebook_helperfunctions.py    
+
+    Params
+    -------
+    screen_name : str
+    no_rt : bool
+    start : datetime-onj
+    end : datetime-obj
+    
+    Returns
+    -------
+    {string} search url for twitter
+    """
+    url1 = 'https://twitter.com/search?f=tweets&q=from%3A'
+    url2 = screen_name + '%20since%3A' + start.strftime('%Y-%m-%d') 
+    url3 = ''
+    if no_rt:
+        url3 = '%20until%3A' + end.strftime('%Y-%m-%d') + '%20&src=typd'
+    else:
+        url3 = '%20until%3A' + end.strftime('%Y-%m-%d') + \
+                '%20include%3Aretweets&src=typd'
+    
+    return url1 + url2 + url3
+
+def get_all_user_tweets(screen_name, start, end, tweet_lim=-1, no_rt=False):
+    """
+    Adapted from: 
+    https://github.com/alejandrox1/tweet_authorship/blob/master/Notebook_helperfunctions.py
+
+    Params
+    ------
+    screen_name : str
+    start : datetime-obj
+    end : datetime-obj
+    no_rt : bool
+    tweet_lim : int {default none / -1}
+    
+    returns
+    -------
+    {int} total number of tweet ids obtained
+    """
+    # Selenium params
+    delay = 1  # time to wait on each page load before reading the page
+    driver = webdriver.Chrome() 
+    
+    all_ids = []
+    ids_total = 0
+    for day in range((end - start).days + 1):
+        # Get Twitter search url
+        startDate = increment_day(start, 0)
+        endDate = increment_day(start, 1)
+        url = twitter_url(screen_name, no_rt, startDate, endDate)
+
+        driver.get(url)
+        time.sleep(delay)
+	
+        try:
+            found_tweets = \
+            driver.find_elements_by_css_selector('li.js-stream-item')
+            increment = 10
+
+            # Scroll through the Twitter search page
+            while len(found_tweets) >= increment:
+                # scroll down for more results
+                driver.execute_script(
+                    'window.scrollTo(0, document.body.scrollHeight);'
+                )
+                time.sleep(delay)
+                # select tweets
+                found_tweets = driver.find_elements_by_css_selector(
+                    'li.js-stream-item'
+                )
+                increment += 10
+
+            # Get the IDs for all Tweets
+            for tweet in found_tweets:
+            	try:
+                	# get tweet id
+                        tweet_id = tweet.find_element_by_css_selector(
+                            '.time a.tweet-timestamp'
+                        ).get_attribute('href').split('/')[-1]
+                        all_ids.append(tweet_id)
+                        ids_total += 1
+                        # break if tweet_lim has been reached                           
+                        if ids_total == tweet_lim:                                   
+                            return ids_total
+
+                except StaleElementReferenceException as e:
+                	print('lost element reference', tweet)
+        
+        except NoSuchElementException:
+            print('no tweets on this day')
+
+        start = increment_day(start, 1)
+    
+    # Close selenium driver
+    driver.close()
+    print('{} tweets found total'.format(ids_total))
+    return all_ids
 
 def upsert_tweets(db,tweets):
     if not tweets:
@@ -142,66 +256,25 @@ def run(consumer_key, consumer_secret, access_key, access_secret,
 
     if not user_id:
         users = user_table.find(tweets_collected=0)
-        user_ids = [u['user_id'] for u in users]
+        user_ids = [u['screen_name'] for u in users]
     else:
         user_ids = []
 
-    if user_id:
-        remaining = 16
-        max_id=None
-        while True:
-            tweets = get_tweets(api,user_id,max_id=max_id)
-
-            tweet_ids = [tweet.id for tweet in tweets]
-            if len(tweet_ids) > 0:
-                max_id = min(tweet_ids)
-                upsert_tweets(db,tweets)
-                update_dict = dict(user_id=user_id,tweets_collected=1)
-                user_table.update(update_dict,['user_id'])
-
-            if len(tweets) < 200:
-                sys.exit('Finished')
-
-            remaining -= 1
-            print(str(remaining) + ' requests to go')
-
-            time.sleep(1)
-
-
     remaining = len(user_ids)
     for user_id in user_ids:
-        print(str(remaining) + ' users to go')
+	print(str(remaining) + ' users to go')
+	print(user_id)
 
         if all_tweets:
-            max_id = None
-            finished = False
-            
-            for i in range(16):
-                if finished:
-                    continue
-                tweets = get_tweets(api,user_id,max_id=max_id)
-                tweet_ids = [tweet.id for tweet in tweets]
-                if len(tweet_ids) > 0:
-                    max_id = min([tweet.id for tweet in tweets])
-                    upsert_tweets(db,tweets)
+            start = datetime.datetime(2016, 1, 1)  
+	    end = datetime.datetime.today()
+	    
+	    tweet_ids = get_all_user_tweets(user_id, start, end)
+  	    
+	    tweet_groups = chunker(tweet_ids,100)
 
-                if len(tweets) < 200:
-                    finished = True
-
-                print('Got ' + str(i) + ' iteration of tweets for ' + str(user_id))
-                time.sleep(1)
-
-            update_dict = dict(screen_name=user_id,tweets_collected=1)
-            user_table.update(update_dict,['user_id'])
-            finished = False
-        else:
-            tweets = get_tweets(api,user_id)
-            upsert_tweets(db,tweets)
-            is_suspended = 0
-            if len(tweets) == 0:
-                is_suspended=1
-            update_dict = dict(screen_name=user_id,tweets_collected=1,is_suspended=is_suspended)
-            user_table.update(update_dict,['user_id'])
+    	    for group in tweet_groups:
+        	temptweets = api.StatusesLookup(group, include_entities=True)
+        	upsert_tweets(db,temptweets)
 
         remaining -= 1
-        time.sleep(1)
--- collect_social/twitter/utils.py
+++ collect_social/twitter/utils.py
@@ -66,5 +66,6 @@ def get_api(consumer_key, consumer_secret, access_key, access_secret):
     api = twitter.Api(consumer_key=consumer_key,
                       consumer_secret=consumer_secret,
                       access_token_key=access_key,
-                      access_token_secret=access_secret)
+                      access_token_secret=access_secret,
+		      sleep_on_rate_limit=True)
     return api
